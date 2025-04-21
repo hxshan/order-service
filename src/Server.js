764066913be93fbd758885e5be8dbb2464ad9de7
Chamod Perera <chamod914@gmail.com>
2025-04-21 20:29:05 +0530

    Initial Setup

--- src/Server.js
+++ src/Server.js
@@ -1,19 +1,30 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const orderRoutes = require('./routes/orderRoutes');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+// const orderRoutes = require('./routes/orderRoutes');
+require("dotenv").config();
 
 const app = express();
+
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to mongoDB"))
+  .catch((err) => console.error("Failed to connect to MongoDB :", err));
 
-app.use('/api/orders', orderRoutes);
+// app.use('/api/orders', orderRoutes);
+
+if (process.env.NODE_ENV !== "test") {
+  app.listen(port, () => {
+    console.log("Server listening on port:", port);
+  });
+}
 
 module.exports = app;
